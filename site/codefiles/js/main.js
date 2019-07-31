ff7fdf24f0c788599075b1248d85ace462b5fd57
kd <krtdvn@gmail.com>
2019-07-31 16:27:07 +0530

    fixes

--- site/codefiles/js/main.js
+++ site/codefiles/js/main.js
@@ -34,24 +34,7 @@ function items_loaded(){
   
   Vue.component('shop-item',{
     props: ['name','image','category','unitprice','unit','instock','description','quantity','compact'],
-    template: `
-      <div class="shopitem">
-        <img v-if="!compact" src="/inventory/images/b1.jpg">
-        <span class="name">{{name}}</span>
-        <span class="price">{{$parent.settings.currency}}{{unitprice}}/{{unit}}</span>
-        <button v-on:click="$emit('plus')" v-if="instock=='yes' && quantity<1">Add to cart</button>
-        <div class="qctrl" v-else-if="instock=='yes' && quantity>0">
-          <span class="icon" v-on:click="$emit('minus')">r</span>
-          <span>{{quantity}}</span>
-          <span class="icon" v-on:click="$emit('plus')">a</span>
-        </div>
-        <span v-else class="outofstock">Out of Stock</span>
-        <details v-if="!compact">
-          <summary>Description</summary>
-          {{description}}
-        </details>
-      </div>
-    `
+    template: '#shop-item-template'
   });
   
   app = new Vue({
@@ -62,11 +45,12 @@ function items_loaded(){
       settings: settings,
       cartpage: false,
       name: '',
+      matches: true
     },
     computed: {
       carttotal: function(){
         var t = 0;
-        for(var i=0;i<this.items.length;c++){
+        for(var i=0;i<this.items.length;i++){
           t += this.items[i].quantity*this.items[i]['Unit Price'];
         }
         return t;
@@ -97,15 +81,17 @@ function items_loaded(){
         location.href = `sms:${app.settings.shopphone}?body=${encodeURIComponent(app.ordertext)}`;
       },
       orderemail: function(){
-        location.href = `mailto:${app.settings.shopemail}?subject=Order%20from%20${name}&body=${encodeURIComponent(app.ordertext)}`;
+        location.href = `mailto:${app.settings.shopemail}?subject=Order%20from%20${app.name}&body=${encodeURIComponent(app.ordertext)}`;
       },
       search: function(){
         var query = document.querySelector('#searchquery').value;
         query = query.trim().toLowerCase();
+        app.matches = false;
         for(var i=0;i<app.items.length;i++){
           var target = app.items[i].Name.toLowerCase();
           if(target.includes(query)){
             app.items[i].match = true;
+            app.matches = true;
           }
           else{
             app.items[i].match = false;
