f0b605918f57b9990a68d383815e5d73b6871445
Karthik D <krtdvn@gmail.com>
2020-05-09 19:40:19 +0530

    Update settings.js

--- site/codefiles/js/settings.js
+++ site/codefiles/js/settings.js
@@ -2,6 +2,6 @@ let settings = {
   showOutOfStockItems: true,
   showDescription: true,
   currency: '₹',
-  shopphone: '919901297970,
+  shopphone: '919901297970',
   shopemail: 'hi@wiosdesigns.xyz'
 };
